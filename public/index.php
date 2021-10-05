5bb0d755ff1c6fd29dd69ed4c2895a4de0a9f04d
Katarzyna Krasińska <katheroine@users.noreply.github.com>
2021-10-05 20:42:13 +0200

    Revert "Header created [katheroine#3]"

--- public/index.php
+++ public/index.php
@@ -1,14 +1,8 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
-use Twig\Loader\FilesystemLoader;
-use Twig\Environment;
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, ['debug' => true]);
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
-$twig = new Environment($loader, ['debug' => true]);
-
-$template = $twig->load('index.twig.html');
-echo $template->render([
-  'base_dir' => '.',
-  'debug' => false,
-]);
+$template = $twig->load('index.thtml');
+echo $template->render([]);
