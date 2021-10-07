081b36380287e7aed1ca959bbc9926025e4b7308
katheroine <katheroine@gmail.com>
2021-10-07 10:13:13 +0200

    GitHub Pages generated [katheroine#3]

---
+++ public/index.githubpages.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader, ['debug' => true]);
+
+$template = $twig->load('index.twig.html');
+echo $template->render([
+  'base_dir' => './public',
+  'debug' => false,
+]);
