a82d0aba5e0b18410ffde0533e84af03c6b7b262
katheroine <katheroine@gmail.com>
2022-04-21 13:51:08 +0200

    GitHub Pages updated [katheroine#16]

--- public/demo/generators/index.demo.php
+++ public/demo/generators/index.demo.php
@@ -1,16 +1,16 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
+$loader = new FilesystemLoader(__DIR__ . '/../../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
 $template = $twig->load('index.twig.html');
 echo $template->render([
   'base_url' => '.',
-  'assets_dir' => './public',
+  'assets_dir' => '..',
   'code_file_extension' => 'html',
   'debug' => false,
 ]);
---
+++ public/demo/generators/layouts_board_one_column.demo.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader(__DIR__ . '/../../templates');
+$twig = new Environment($loader, ['debug' => true]);
+
+$template = $twig->load('layouts/board/one_column.twig.html');
+echo $template->render([
+  'base_url' => '../..',
+  'assets_dir' => '../../..',
+  'code_file_extension' => 'html',
+  'debug' => false,
+]);
