2f2b260c075f1f6c4d5484c7405471463734d0b5
katheroine <katheroine@gmail.com>
2022-05-18 18:34:27 +0200

    Demo (GitHub Pages) moved out of public directory [katheroine#21]

--- demo/generators/index.demo.php
+++ demo/generators/index.demo.php
@@ -1,16 +1,16 @@
 <?php
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . '/../../templates');
+$loader = new FilesystemLoader(__DIR__ . '/../../public/templates');
 $twig = new Environment($loader, ['debug' => true]);
 
 $template = $twig->load('index.twig.html');
 echo $template->render([
   'base_url' => '.',
-  'subpages_url' => './public/demo',
+  'subpages_url' => './demo',
   'assets_dir' => './public',
   'code_file_extension' => 'html',
   'debug' => false,
--- demo/generators/layouts_board_one_column.demo.php
+++ demo/generators/layouts_board_one_column.demo.php
@@ -1,17 +1,17 @@
 <?php
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . '/../../templates');
+$loader = new FilesystemLoader(__DIR__ . '/../../public/templates');
 $twig = new Environment($loader, ['debug' => true]);
 
 $template = $twig->load('layouts/board/one_column.twig.html');
 echo $template->render([
-  'base_url' => '../../../..',
-  'subpages_url' => '../../../demo',
-  'assets_dir' => '../../..',
+  'base_url' => '../../..',
+  'subpages_url' => '../..',
+  'assets_dir' => '../../../public',
   'code_file_extension' => 'html',
   'debug' => false,
 ]);
