c29b8e7103ecc2dfe2514629ae457a3cc2823ded
katheroine <katheroine@gmail.com>
2022-05-20 20:08:18 +0200

    Navigation links customizability [katheroine#21]

--- public/pages/index.php
+++ public/pages/index.php
@@ -3,15 +3,28 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Symfony\Component\Yaml\Yaml;
 
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
+$navigation_links_config_file_path = '../../config/navigation_links.yaml';
+$navigation_links_config_content = file_get_contents($navigation_links_config_file_path);
+$navigation_links_config_replacements = [
+  '[[base_url]]' => '../../..',
+  '[[code_file_extension]]' => 'php',
+];
+$navigation_links_config_content_prepared = str_replace(
+  array_keys($navigation_links_config_replacements),
+  array_values($navigation_links_config_replacements),
+  $navigation_links_config_content
+);
+$navigation_links = Yaml::parse($navigation_links_config_content_prepared);
+
 $template = $twig->load('index.default.twig.html');
 echo $template->render([
-  'base_url' => '.',
   'subpages_url' => './pages',
   'assets_dir' => '../assets',
-  'code_file_extension' => 'php',
+  'navigation_links' => $navigation_links,
   'debug' => true,
 ]);
