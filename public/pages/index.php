e5ba62d187ef583eac99349a47e015635a8ea3ec
katheroine <katheroine@gmail.com>
2022-05-20 16:48:33 +0200

    Pages moved to the pages directory, Apache2 site conf example added [katheroine#21]

--- public/pages/index.php
+++ public/pages/index.php
@@ -1,17 +1,17 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
 $template = $twig->load('index.default.twig.html');
 echo $template->render([
   'base_url' => '.',
-  'subpages_url' => '.',
-  'assets_dir' => './assets',
+  'subpages_url' => './pages',
+  'assets_dir' => '../assets',
   'code_file_extension' => 'php',
   'debug' => true,
 ]);
--- public/pages/layouts/board/one_column.php
+++ public/pages/layouts/board/one_column.php
@@ -1,17 +1,17 @@
 <?php
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . '/../../templates');
+$loader = new FilesystemLoader(__DIR__ . '/../../../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
 $template = $twig->load('layouts/board/one_column.twig.html');
 echo $template->render([
-  'base_url' => '../..',
+  'base_url' => '../../..',
   'subpages_url' => '../..',
-  'assets_dir' => '../../assets',
+  'assets_dir' => '../../../assets',
   'code_file_extension' => 'php',
   'debug' => false,
 ]);
