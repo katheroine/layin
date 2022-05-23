40bd9a640dd5883e235fd91c2e814641006ad303
katheroine <katheroine@gmail.com>
2022-05-23 17:48:23 +0200

    Template base.basic parametrized with site configuration [katheroine#21]

--- public/pages/index.php
+++ public/pages/index.php
@@ -8,6 +8,8 @@ use Symfony\Component\Yaml\Yaml;
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
+$site_config = Yaml::parseFile('../../config/site_config.yaml');
+
 $navigation_links_config_file_path = '../../config/navigation_links.yaml';
 $navigation_links_config_content = file_get_contents($navigation_links_config_file_path);
 $navigation_links_config_replacements = [
@@ -25,11 +27,16 @@ $contact_info_links_config_file_path = '../../config/contact_info_links.yaml';
 $contact_info_links_config_content = file_get_contents($contact_info_links_config_file_path);
 $contact_info_links = Yaml::parse($contact_info_links_config_content);
 
+$template_params = array_merge(
+  $site_config,
+  [
+    'subpages_url' => './pages',
+    'assets_dir' => '../assets',
+    'navigation_links' => $navigation_links,
+    'contact_info_links' => $contact_info_links,
+    'debug' => false,
+  ]
+);
+
 $template = $twig->load('index.default.twig.html');
-echo $template->render([
-  'subpages_url' => './pages',
-  'assets_dir' => '../assets',
-  'navigation_links' => $navigation_links,
-  'contact_info_links' => $contact_info_links,
-  'debug' => true,
-]);
+echo $template->render($template_params);
--- public/pages/layouts/board/one_column.php
+++ public/pages/layouts/board/one_column.php
@@ -8,12 +8,12 @@ use Symfony\Component\Yaml\Yaml;
 $loader = new FilesystemLoader(__DIR__ . '/../../../templates');
 $twig = new Environment($loader, ['debug' => true]);
 
-$base_url = '../../..';
+$site_config = Yaml::parseFile('../../../../config/site_config.yaml');
 
 $navigation_links_config_file_path = '../../../../config/navigation_links.yaml';
 $navigation_links_config_content = file_get_contents($navigation_links_config_file_path);
 $navigation_links_config_replacements = [
-  '[[base_url]]' => $base_url,
+  '[[base_url]]' => '../../..',
   '[[code_file_extension]]' => 'php',
 ];
 $navigation_links_config_content_prepared = str_replace(
@@ -27,12 +27,16 @@ $contact_info_links_config_file_path = '../../../../config/contact_info_links.ya
 $contact_info_links_config_content = file_get_contents($contact_info_links_config_file_path);
 $contact_info_links = Yaml::parse($contact_info_links_config_content);
 
+$template_params = array_merge(
+  $site_config,
+  [
+    'subpages_url' => '../..',
+    'assets_dir' => '../../../assets',
+    'navigation_links' => $navigation_links,
+    'contact_info_links' => $contact_info_links,
+    'debug' => false,
+  ]
+);
+
 $template = $twig->load('layouts/board/one_column.twig.html');
-echo $template->render([
-  'base_url' => $base_url,
-  'subpages_url' => '../..',
-  'assets_dir' => '../../../assets',
-  'navigation_links' => $navigation_links,
-  'contact_info_links' => $contact_info_links,
-  'debug' => false,
-]);
+echo $template->render($template_params);
