678906b2732021c4604a632b51936ffac521364e
katheroine <katheroine@gmail.com>
2022-06-21 21:33:07 +0200

    GitHub Pages updated [katheroine#21]

--- demo/generators/index.demo.php
+++ demo/generators/index.demo.php
@@ -1,17 +1,9 @@
 <?php
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Twig\Loader\FilesystemLoader;
-use Twig\Environment;
+use Katheroine\Layin\Demo\IndexPreconfiguredPageRenderer;
 
-$loader = new FilesystemLoader(__DIR__ . '/../../public/templates');
-$twig = new Environment($loader, ['debug' => true]);
+$pageRenderer = new IndexPreconfiguredPageRenderer();
+$pageRenderer->renderPreconfiguredPage('index.layin.twig.html');
 
-$template = $twig->load('index.twig.html');
-echo $template->render([
-  'base_url' => '.',
-  'subpages_url' => './demo',
-  'assets_dir' => './public/assets',
-  'code_file_extension' => 'html',
-  'debug' => false,
-]);
--- demo/generators/layouts_board_one_column.demo.php
+++ demo/generators/layouts_board_one_column.demo.php
@@ -1,17 +1,8 @@
 <?php
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Twig\Loader\FilesystemLoader;
-use Twig\Environment;
+use Katheroine\Layin\Demo\LayoutsBoardPreconfiguredPageRenderer;
 
-$loader = new FilesystemLoader(__DIR__ . '/../../public/templates');
-$twig = new Environment($loader, ['debug' => true]);
-
-$template = $twig->load('layouts/board/one_column.twig.html');
-echo $template->render([
-  'base_url' => '../../..',
-  'subpages_url' => '../..',
-  'assets_dir' => '../../../public/assets',
-  'code_file_extension' => 'html',
-  'debug' => false,
-]);
+$pageRenderer = new LayoutsBoardPreconfiguredPageRenderer();
+$pageRenderer->renderPreconfiguredPage('one_column.layin.twig.html');
