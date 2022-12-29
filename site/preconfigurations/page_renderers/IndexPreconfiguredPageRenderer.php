e8229e0f77ab3765487ff3555d0ffeebbba10067
katheroine <katheroine@gmail.com>
2022-12-29 19:34:39 +0100

    Concrete Layin preconfigured page renderers updated to be based on the new base [katheroine#33]

--- site/preconfigurations/page_renderers/IndexPreconfiguredPageRenderer.php
+++ site/preconfigurations/page_renderers/IndexPreconfiguredPageRenderer.php
@@ -11,21 +11,35 @@
 
 namespace Katheroine\Layin\Preconfiguration;
 
-use Katheroine\Layin\Preconfigurator\VioletPageRendererPreconfigurator;
+// use Katheroine\Layin\Preconfigurator\VioletPageRendererPreconfigurator;
 
-class IndexPreconfiguredPageRenderer extends VioletPageRendererPreconfigurator
+class IndexPreconfiguredPageRenderer extends AbstractBasePreconfiguredPageRenderer
 {
     protected function providePreconfiguration(): array
     {
+        // return [
+        // 'templates_dir_absolute_path' => __DIR__ . '/../../templates',
+        // 'template_local_path' => '',
+        // 'config_dir_path' => '../../config',
+        // 'base_url' => '../..',
+        // 'subpages_relative_url' => 'pages',
+        // 'assets_dir_relative_path' => '../assets',
+        // 'code_file_extension' => 'php',
+        // 'is_debug_mode' => false,
+        // ];
+
         return [
-        'templates_dir_absolute_path' => __DIR__ . '/../../templates',
-        'template_local_path' => '',
-        'config_dir_path' => '../../config',
-        'base_url' => '../..',
-        'subpages_relative_url' => 'pages',
-        'assets_dir_relative_path' => '../assets',
-        'code_file_extension' => 'php',
-        'is_debug_mode' => false,
+            'templates_dir_path' => __DIR__ . '/../../templates',
+            'template_subdir_path' => '',
+            'template_file_extension' => 'twig.html',
+            'page_file_extension' => 'php',
+            'site_config_path' => '../../config/site_config.yaml',
+            'navigation_links_config_path' => '../../config/navigation_links.yaml',
+            'contact_info_links_config_path' => '../../config/contact_info_links.yaml',
+            'base_url' => '.',
+            'subpages_url' => 'pages',
+            'assets_dir_path' => '../assets',
+            'is_debug_mode' => false,
         ];
     }
 }
